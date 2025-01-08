d598d03f696caf6d920919ed0e53eb0ead171806
Super_Javan <296652579@qq.com>
2025-01-08 17:59:52 +0800

    优化导航线路 任意角度射线检测和移动

--- assets/module_movecar/RoosterMoveCar.ts
+++ assets/module_movecar/RoosterMoveCar.ts
@@ -49,8 +49,10 @@ export class RoosterMoveCar extends Component {
         let results = PhysicsSystem2D.instance.raycast(objs, obje, ERaycast2DType.Closest);
         if (results.length > 0) {
             const closestResult = results[0];
-            console.log('碰撞结果:', closestResult.collider.node.name);
+
             const collider = results[0].collider;
+            console.log('碰撞器名称:', collider.name);
+            console.log('碰撞点世界坐标:', results[0].point);
             //碰到车
             if (collider.group == 1 << 1) {
                 const halfLen = carComp.halfLen;
@@ -80,20 +82,24 @@ export class RoosterMoveCar extends Component {
                     return
                 }
 
+                const targetWorldPos = results[0].point;
                 // 顶部导航
                 let tweenCar: Tween<Node> = tween(car)
                 if (collider.node.name === "PhysicRoodTop") {
-                    this.hitPointTween(car, parkPoint, tweenCar, collider.node)
+                    // this.hitPointTween(car, parkPoint, tweenCar, collider.node)
+                    this.hitPointTween2(car, targetWorldPos, tweenCar, collider.node)
                     this.topRoadTween(car, parkPoint, tweenCar)
                 } else if (collider.node.name === "PhysicRoodLeft") {
                     const targetPoint = find("Canvas/Scene/Grounds/PhysicRoodTop/LeftPoint")
-                    this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                    // this.hitPointTween(car, targetPoint, tweenCar, collider.node);
+                    this.hitPointTween2(car, targetWorldPos, tweenCar, collider.node)
                     this.leftRoadTween(car, tweenCar)
                     this.leftTopToRight(car, parkPoint, tweenCar)
                     this.topRoadTween(car, parkPoint, tweenCar)
                 } else if (collider.node.name === "PhysicRoodRight") {
                     const targetPoint = find("Canvas/Scene/Grounds/PhysicRoodTop/RightPoint")
-                    this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                    // this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                    this.hitPointTween2(car, targetWorldPos, tweenCar, collider.node)
                     this.rightRoadTween(car, tweenCar)
                     this.rightTopToleft(car, parkPoint, tweenCar)
                     this.topRoadTween(car, parkPoint, tweenCar)
@@ -106,13 +112,15 @@ export class RoosterMoveCar extends Component {
 
                     if (Math.abs(toLeft.x) < Math.abs(toRight.x)) {
                         const targetPoint = find("Canvas/Scene/Grounds/PhysicRoodBottom/LeftPoint")
-                        this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                        // this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                        this.hitPointTween2(car, targetWorldPos, tweenCar, collider.node)
                         this.bottomRoadTween(car, targetPoint, tweenCar)
                         this.leftRoadTween(car, tweenCar)
                         this.leftTopToRight(car, parkPoint, tweenCar)
                     } else {
                         const targetPoint = find("Canvas/Scene/Grounds/PhysicRoodBottom/RightPoint")
-                        this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                        // this.hitPointTween(car, targetPoint, tweenCar, collider.node)
+                        this.hitPointTween2(car, targetWorldPos, tweenCar, collider.node)
                         this.bottomRoadTween(car, targetPoint, tweenCar)
                         this.rightRoadTween(car, tweenCar)
                         this.rightTopToleft(car, parkPoint, tweenCar)
@@ -179,9 +187,8 @@ export class RoosterMoveCar extends Component {
         } else if (rotation === 180) {
             direction = v2(0, -1); // 朝下
         } else {
-            // 如果是任意角度，使用 Math.cos 和 Math.sin 计算方向向量
-            const radians = rotation * (Math.PI / 180);
-            direction = v2(Math.cos(radians), Math.sin(radians));
+            const adjustedAngle = rotation - 90;
+            direction = v2(-Math.cos(adjustedAngle * (Math.PI / 180)), Math.sin(-adjustedAngle * (Math.PI / 180)));
         }
 
         // 计算射线起点坐标
@@ -195,36 +202,19 @@ export class RoosterMoveCar extends Component {
     }
 
     /** 导航到碰撞点
-     *@param targetPoint 下一个目标点
+     * @param car 车节点
+     *@param targetWorldPos 碰撞点的世界坐标
+     *@param tweenCar 车的tween动画
      *@param hitPoint street碰撞点
     */
-    hitPointTween(car: Node, targetPoint: Node, tweenCar: Tween<Node>, hitPoint: Node = null) {
-        const newHintStreetPos = hitPoint.getWorldPosition().clone();
-        const carWorldPos = car.getWorldPosition();
+    hitPointTween2(car: Node, targetWorldPos: Vec2, tweenCar: Tween<Node>, hitPoint: Node = null) {
+        const targetV3 = new Vec3(targetWorldPos.x, targetWorldPos.y, 0);
 
-        const carDir = car.getComponent(CarCarColorsComponent)!.carDir;
-        if (carDir == CarDir.LEFT || carDir == CarDir.RIGHT) {
-            // 判断移动方向
-            if (Math.abs(newHintStreetPos.x - carWorldPos.x) > Math.abs(newHintStreetPos.y - carWorldPos.y)) {
-                // 如果 X 轴差值更大，只改变 X 轴
-                newHintStreetPos.x = newHintStreetPos.x;
-                newHintStreetPos.y = carWorldPos.y;
-            } else {
-                // 否则只改变 Y 轴
-                newHintStreetPos.x = carWorldPos.x;
-                newHintStreetPos.y = newHintStreetPos.y;
-            }
-        } else if (carDir == CarDir.TOP || carDir == CarDir.BOTTOM) {
-            newHintStreetPos.x = carWorldPos.x;
-            newHintStreetPos.y = newHintStreetPos.y;
-        }
-
-        tweenCar.to(0.2, {
-            worldPosition: newHintStreetPos
-        })
+        tweenCar
+            .to(0.2, { worldPosition: targetV3 }, { easing: 'quadInOut' })
             .call(() => {
-                const targetWorldPos = targetPoint.getWorldPosition();
-                const direction = targetWorldPos.subtract(newHintStreetPos).normalize();
+                const carWorldPos = car.getWorldPosition().clone();
+                const direction = targetV3.subtract(carWorldPos).normalize();
                 // console.log('direction:', direction);
 
                 let up: Vec3;
@@ -242,40 +232,90 @@ export class RoosterMoveCar extends Component {
                     }
                 } else {
                     // 上下方向
-                    up = direction.y > 0 ? new Vec3(0, 1, 0) : new Vec3(0, -1, 0);
+                    up = direction.y > 0 ? new Vec3(0, -1, 0) : new Vec3(0, 1, 0);
                 }
-                car.lookAt(targetWorldPos, up);
+                car.lookAt(targetV3, up);
             })
-            .delay(0.1)
             .start();
-
-        // const targetPosition = hitPoint.getWorldPosition().clone();
-        // const rotation = car.angle;
-        // console.log('当前车的角度:', rotation);
-
-        // const radians = rotation * (Math.PI / 180);
-        // const direction = v3(Math.cos(radians), Math.sin(radians), 0);//方向向量
-
-        // // 计算从当前位置到目标位置的向量
-        // const currentPosition = this.node.position;
-        // const toTarget = targetPosition.subtract(currentPosition);
-
-        // const speed = 200;
-        // const moveDistance = direction.multiplyScalar(speed);
-
-        // // 计算移动时间（根据目标位置和移动距离）
-        // const distance = toTarget.length();
-        // const duration = distance / speed;
-
-        // tween(this.node.position)
-        //     .to(duration, toTarget, {
-        //         onUpdate: (target: Vec3, ratio: number) => {
-        //             // 更新车的位置
-        //             this.node.position = currentPosition.add(moveDistance.multiplyScalar(ratio));
-        //         }
-        //     })
-        //     .start();
     }
+
+    carDirection(car: Node) {
+        const rotation = car.angle;
+
+        // 计算移动方向
+        let direction = v3(0, 0, 0);
+        if (rotation === -90) {
+            direction = v3(1, 0, 0); // 朝右
+        } else if (rotation === 0) {
+            direction = v3(0, 1, 0); // 朝上
+        } else if (rotation === 90) {
+            direction = v3(-1, 0, 0); // 朝左
+        } else if (rotation === 180) {
+            direction = v3(0, -1, 0); // 朝下
+        } else {
+            // 如果是任意角度，使用 Math.cos 和 Math.sin 计算方向向量
+            const radians = rotation * (Math.PI / 180);
+            direction = v3(Math.cos(radians), Math.sin(radians), 0);
+        }
+
+        return direction;
+    }
+
+    /** 导航到碰撞点
+     *@param targetPoint 下一个目标点
+     *@param hitPoint street碰撞点
+    */
+    // hitPointTween(car: Node, targetPoint: Node, tweenCar: Tween<Node>, hitPoint: Node = null) {
+    //     const newHintStreetPos = hitPoint.getWorldPosition().clone();
+    //     const carWorldPos = car.getWorldPosition();
+
+    //     const carDir = car.getComponent(CarCarColorsComponent)!.carDir;
+    //     if (carDir == CarDir.LEFT || carDir == CarDir.RIGHT) {
+    //         // 判断移动方向
+    //         if (Math.abs(newHintStreetPos.x - carWorldPos.x) > Math.abs(newHintStreetPos.y - carWorldPos.y)) {
+    //             // 如果 X 轴差值更大，只改变 X 轴
+    //             newHintStreetPos.x = newHintStreetPos.x;
+    //             newHintStreetPos.y = carWorldPos.y;
+    //         } else {
+    //             // 否则只改变 Y 轴
+    //             newHintStreetPos.x = carWorldPos.x;
+    //             newHintStreetPos.y = newHintStreetPos.y;
+    //         }
+    //     } else if (carDir == CarDir.TOP || carDir == CarDir.BOTTOM) {
+    //         newHintStreetPos.x = carWorldPos.x;
+    //         newHintStreetPos.y = newHintStreetPos.y;
+    //     }
+
+    //     tweenCar.to(0.2, {
+    //         worldPosition: newHintStreetPos
+    //     })
+    //         .call(() => {
+    //             const targetWorldPos = targetPoint.getWorldPosition();
+    //             const direction = targetWorldPos.subtract(newHintStreetPos).normalize();
+    //             // console.log('direction:', direction);
+
+    //             let up: Vec3;
+    //             if (Math.abs(direction.x) > Math.abs(direction.y)) {
+    //                 // 左右方向
+    //                 const leftPoint = hitPoint.getChildByName('LeftPoint')!;
+    //                 const rightPoint = hitPoint.getChildByName('RightPoint')!;
+    //                 const toLeft = leftPoint.getWorldPosition().subtract(carWorldPos).normalize();
+    //                 const toRight = rightPoint.getWorldPosition().subtract(carWorldPos).normalize();
+
+    //                 if (Math.abs(toLeft.x) < Math.abs(toRight.x)) {
+    //                     up = new Vec3(0, 0, 1);
+    //                 } else {
+    //                     up = new Vec3(0, 0, -1);
+    //                 }
+    //             } else {
+    //                 // 上下方向
+    //                 up = direction.y > 0 ? new Vec3(0, 1, 0) : new Vec3(0, -1, 0);
+    //             }
+    //             car.lookAt(targetWorldPos, up);
+    //         })
+    //         .delay(0.1)
+    //         .start();
+    // }
 
     // 顶部导航
     topRoadTween(car: Node, targetPoint: Node, tweenCar: Tween<Node>) {
